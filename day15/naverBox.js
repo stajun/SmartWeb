cf11d9ffa31a15e16d423b6eb154e709142cf4d0
Unknown <jrj8819@koreatech.ac.kr>
2018-12-17 16:43:03 +0900

    18회차:제이쿼리-네이버 더보기 기능 완료

--- day15/naverBox.js
+++ day15/naverBox.js
@@ -4,18 +4,20 @@ $(document).ready(function(){
 		$('.auto-down').toggleClass('auto-up');
 	});
 	$('.item4').click(function(){
+		//접기 또는 더보기 버튼을 클릭하면 메뉴에 있는 배열을 임시 배열에 저장한다.
+		selectedMenu = menuArr2.slice();
+		displayMenu();
 		menu();
+		checkMenu();
+		closeSubMenu();
+		initCheck();
 	});
-	$('.menu-close').click(function(){
-		menu();
-		// $('.sub-menu').removeClass('display-block');
-		// $('.sub-menu-background').removeClass('display-block');
-	});
-	function menu(){
-		$('.item4').toggleClass('item4-1');
-		$('.sub-menu').toggleClass('display-block');
-		$('.sub-menu-background').toggleClass('display-block');
-	}
+	// $('.menu-close').click(function(){
+	// 	menu();
+	// 	// $('.sub-menu').removeClass('display-block');
+	// 	// $('.sub-menu-background').removeClass('display-block');
+	// });
+	
 	//div는 ul태그를 감싸는 객체의 선택자명
 	//h는 marginTop의 높이
 	//time은 이동하는데 걸리는 시간
@@ -86,28 +88,184 @@ $(document).ready(function(){
 	});
 	$('.rank-lists').first().css('display','block');
 	$('.rank-lists').last().css('display','none');
-
-	var selectedMenuCnt = 0;
+	/* 기본 메뉴 */
 	var menuArr = ["dici", "newsi", "stocki", "dealeri", "mapi", 
 		"moviei", "musici", "booki", "webtooni"];
+	
 	$('.menu-setting').click(function(){
+		createCheck();
+		/* 메뉴 설정 클릭 시 5개의 빈 박스를 보이기 위한 cnt */
 		var cnt = 0;
 		$('.item2-1').each(function(){
-			$(this).prop('class','item2-1');
-			cnt++;
-			if(cnt > 5){
-				$(this).addClass('display-none');
+			/* 기본 코드에 item2-1과 back-img와 각 아이콘 클래스가 있는데
+				back-img와 아이콘 클래스를 제거하기 위해 prop함수로
+				class를 덮어쓰기한다.
+			*/
+			if(menuArr2.length <= cnt){
+				$(this).prop('class','item2-1');
+				if(cnt > 4){
+					$(this).addClass('display-none');
+				}	
 			}
+			cnt++;
 		});
+		$('.sub-menu-div input[type=checkbox]').each(function(){
+			$(this).removeClass('display-none');
+		});
+		$('.all-service').addClass('display-none');
+		$('.menu-setting').addClass('display-none');
+		$('.ok').removeClass('display-none');
+		$('.init').removeClass('display-none');
+		$('.cancel').removeClass('display-none');
 	});
 	$('.menu-close').click(function(){
-		var i =0;
-		if(selectedMenuCnt == 0){
+		selectedMenu = [];
+		checkMenu();
+		menu();
+		closeSubMenu();
+		initCheck();
+	});
+
+	$('.cancel').click(function(){
+		initCheck();
+		checkMenu();
+		selectedMenu = menuArr2.slice();
+		$('.all-service').removeClass('display-none');
+		$('.menu-setting').removeClass('display-none');
+		$('.ok').addClass('display-none');
+		$('.init').addClass('display-none');
+		$('.cancel').addClass('display-none');
+		var i = 0;
+		if(menuArr2.length == 0){
 			$('.item2-1').each(function(){
 				$(this).prop('class','item2-1 back-img');
 				$(this).addClass(menuArr[i++]);
 			});
+		}else{
+			$('.item2-1').each(function(){
+				if(menuArr2.length > i){
+					$(this).prop('class','item2-1 back-img');
+					$(this).addClass(menuArr2[i++]);
+				}else{
+					$(this).prop('class','item2-1 display-none');
+				}
+			});
 		}
+		$('.sub-menu-div input[type=checkbox]').each(function(){
+			$(this).addClass('display-none');
+		});
 	});
+	/* 메뉴 설정에서 선택한 메뉴들을 저장하는 배열 */
+	var selectedMenu = [];
+	/* 실제 네이버에서 뿌려줄 메뉴 */
+	var menuArr2=[];
+	$('.sub-menu-div input[type="checkbox"]').click(function(){
+		//클릭한 체크박스의 value를 가져옴
+		var check = $(this);
+		//배열에 해당 체크박스의 value가 있는지를 확인
+		var isContain = selectedMenu.indexOf(check.val());
+		var maxSize = 5;
+		//체크 박스의 value가 배열에 없고 배열의 길이가 2이면 
+		//해당 체크박스의 체크를 비활성화
+		if(isContain<0 && selectedMenu.length == maxSize){
+			check.prop('checked','');
+		}
+		//길이가 2가 아니면 해당 배열에 추가를 해당 체크박스의 체크를 활성화
+		else if(isContain<0 && selectedMenu.length != maxSize){
+			selectedMenu.push(check.val());
+			check.prop('checked','checked');
+		}
+		//체크박스의 value가 배열에 있으면 배열에서 해당 문자열을 제거
+		else{
+			selectedMenu.splice(isContain,1);
+		}
+		var cnt = 0;
+		$('.item2-1').each(function(){
+			if(cnt < selectedMenu.length){
+				$(this).prop('class','item2-1 back-img');
+				$(this).addClass(selectedMenu[cnt++]);
+			}
+			else{
+				$(this).prop('class','item2-1');
+				if(cnt > 4)
+					$(this).addClass('display-none');
+				cnt++;
+			}
+		});
+	});
+	$('.ok').click(function(){
+		menuArr2 = selectedMenu.slice();
+		menu();
+		closeSubMenu();
+		initCheck();
+	});
+	$('.init').click(function(){
+		initCheck();
+		menuArr2 = [];
+		selectedMenu = [];
+		alert('초기 설정으로 돌아갑니다.');
+		menu();
+		closeSubMenu();
+		checkMenu();
+	});
+
+	function menu(){
+		$('.item4').toggleClass('item4-1');
+		$('.sub-menu').toggleClass('display-block');
+		$('.sub-menu-background').toggleClass('display-block');
+	}
+	//menuArr2에서 저장된 값들만 check가 되도록 하는 함수
+	function checkMenu(){
+		$('.sub-menu-div input[type=checkbox]').each(function(){
+			$(this).prop('checked', '');
+			for(var i = 0; i<menuArr2.length; i++){
+				if($(this).val() == menuArr2[i]){
+					$(this).prop('checked', 'checked');
+				}
+			}
+		});
+	}
+
+	function closeSubMenu(){
+		displayMenu();
+		$('.sub-menu-div input[type=checkbox]').each(function(){
+			$(this).addClass('display-none');
+		});
+		$('.all-service').removeClass('display-none');
+		$('.menu-setting').removeClass('display-none');
+		$('.ok').addClass('display-none');
+		$('.init').addClass('display-none');
+		$('.cancel').addClass('display-none');
+	}	
+	function initCheck(){
+		$('.sub-menu-div label').each(function(){
+			$(this).prop('for','');
+		})
+	}
+	function createCheck(){
+		var i =0;
+		var checkbox = $('.sub-menu-div input[type=checkbox]');
+		$('.sub-menu-div label').each(function(){
+			$(this).prop('for',checkbox.eq(i++).prop('id'));
+		});
+	}
+	function displayMenu(){
+		var i =0;
+		if(menuArr2.length == 0){
+			$('.item2-1').each(function(){
+				$(this).prop('class','item2-1 back-img');
+				$(this).addClass(menuArr[i++]);
+			});
+		}else{
+			$('.item2-1').each(function(){
+				if(menuArr2.length > i){
+					$(this).prop('class','item2-1 back-img');
+					$(this).addClass(menuArr2[i++]);
+				}else{
+					$(this).prop('class','item2-1 display-none');
+				}
+			});
+		}
+	}
 });
 
