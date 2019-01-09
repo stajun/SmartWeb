482cde8458031b7970fc47675b8e6968a8667e79
Unknown <stajun@naver.com>
2019-01-09 11:28:55 +0900

    23회차:계산기예제-수식 출력 추가

--- day23/calc.js
+++ day23/calc.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 	var pointCnt = 0;
 	var op = '+'; //눌린 연산자를 저장하는 변수
 	var isPushOp = false;//연산자가 눌린적이 있는지 없는지 확인하는 변수
-	var input = $('input');
+	var input = $('#dataNum');
 	var num1 = 0;
+	/* 임시 문자열이 필요한 이유는 =을 제외한 연산자를 연달아 누르면
+	마지막에 누른 연산자가 출력되야 하기 때문에 연산자를 누른 후 다음 숫자가
+	눌리기전까지의 문자열을 임시 문자열에 저장하고 이후 숫자가 누르면 해당 
+	해당 문자열을 최종 문자열에 저장*/
+	var str="";	//최종 문자열
+	var strTmp=""; //임시 문자열
 
 	$('.num').click(function(){
 		var val = $(this).text();
@@ -23,6 +29,7 @@ $(document).ready(function(){
 			input.val(val);
 		else
 			input.val(input.val() + val);
+		str = strTmp;
 	});
 
 	$('.op').click(function(){
@@ -36,9 +43,20 @@ $(document).ready(function(){
 			case '/':	num1 = num1 / tmp; break;
 			case '=':	num1 = tmp; break;
 			}
-			op = $(this).text();
 			input.val(num1);
+			//1+2+-
+			//연산자가 처음 눌리면 
+			str = str + tmp;
 		}
+		op = $(this).text();
+		strTmp = str + op;
+		
+		$('#dataStr').val(strTmp);
+		if(op == '='){
+			str="";
+			strTmp="";
+		}
+		
 		isPushOp = true;
 	});
 });
